eadf27182b65916dfcf3487303de75df0a47daf5
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-24 17:37:08 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/socmed/profile/SocmedProfileApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar.socmed.socmedprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocmedProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocmedProfileApplication.class, args);
+	}
+}
